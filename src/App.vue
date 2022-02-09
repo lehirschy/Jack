18495d5a3d62f01e1930f0ea407a41dbd9779cb8
lehirschy <edensjunk@yahoo.com>
2022-02-09 15:00:00 -0600

    commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+<template>
+  <p class="w-1/2 mx-auto border border-black py-8 mt-24 text-3xl text-center bg-red-300  font-bold underline">
+    Hello world!
+  </p>
+</template>
